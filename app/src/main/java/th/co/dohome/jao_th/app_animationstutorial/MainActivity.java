8d15aaf0ecb33adb3057c3c1bc0536f30bfa5e63
withawat.bun <mooartskyline@gmail.com>
2018-04-19 10:44:19 +0700

    'fitstcommit'

---
+++ app/src/main/java/th/co/dohome/jao_th/app_animationstutorial/MainActivity.java
@@ -0,0 +1,13 @@
+package th.co.dohome.jao_th.app_animationstutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
